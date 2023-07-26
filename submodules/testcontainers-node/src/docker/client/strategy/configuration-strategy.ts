fbdde4630e966c9f005a26f8e7dd792d0ef3e4f0
Daria Terteac <dterteac@alliedtesting.com>
2023-07-26 10:27:13 +0300

    K-15

---
+++ submodules/testcontainers-node/src/docker/client/strategy/configuration-strategy.ts
@@ -0,0 +1,60 @@
+import { getDockerClientConfig } from "../docker-client-config";
+import Dockerode, { DockerOptions } from "dockerode";
+import { sessionId } from "../../session-id";
+import { URL } from "url";
+import { promises as fs } from "fs";
+import path from "path";
+import { DockerClientStrategyResult } from "../docker-client";
+import { DockerClientStrategy } from "./docker-client-strategy";
+
+export class ConfigurationStrategy implements DockerClientStrategy {
+  private dockerHost!: string;
+  private dockerTlsVerify: string | undefined;
+  private dockerCertPath: string | undefined;
+
+  async init(): Promise<void> {
+    const { dockerHost, dockerTlsVerify, dockerCertPath } = await getDockerClientConfig();
+    if (dockerHost) {
+      this.dockerHost = dockerHost;
+    }
+    this.dockerTlsVerify = dockerTlsVerify;
+    this.dockerCertPath = dockerCertPath;
+  }
+
+  async getDockerClient(): Promise<DockerClientStrategyResult> {
+    const dockerOptions: DockerOptions = { headers: { "x-tc-sid": sessionId } };
+
+    const { pathname, hostname, port } = new URL(this.dockerHost);
+    if (hostname !== "") {
+      dockerOptions.host = hostname;
+      dockerOptions.port = port;
+    } else {
+      dockerOptions.socketPath = pathname;
+    }
+
+    if (this.dockerTlsVerify === "1" && this.dockerCertPath !== undefined) {
+      dockerOptions.ca = await fs.readFile(path.resolve(this.dockerCertPath, "ca.pem"));
+      dockerOptions.cert = await fs.readFile(path.resolve(this.dockerCertPath, "cert.pem"));
+      dockerOptions.key = await fs.readFile(path.resolve(this.dockerCertPath, "key.pem"));
+    }
+
+    return {
+      uri: this.dockerHost,
+      dockerode: new Dockerode(dockerOptions),
+      composeEnvironment: {
+        DOCKER_HOST: this.dockerHost,
+        DOCKER_TLS_VERIFY: this.dockerTlsVerify,
+        DOCKER_CERT_PATH: this.dockerCertPath,
+      },
+      allowUserOverrides: true,
+    };
+  }
+
+  isApplicable(): boolean {
+    return this.dockerHost !== undefined;
+  }
+
+  getName(): string {
+    return "ConfigurationStrategy";
+  }
+}
---
+++ submodules/testcontainers-node/src/docker/remote-docker-unix-socket-path.ts
@@ -0,0 +1,24 @@
+import { DockerClient } from "./client/docker-client";
+
+export const getRemoteDockerUnixSocketPath = (
+  dockerClient: DockerClient,
+  platform: NodeJS.Platform = process.platform,
+  env: NodeJS.ProcessEnv = process.env
+): string => {
+  if (dockerClient.allowUserOverrides) {
+    if (env["TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE"] !== undefined) {
+      return env["TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE"];
+    }
+  }
+
+  let socketPath: string;
+  if (dockerClient.info.dockerInfo.operatingSystem === "Docker Desktop") {
+    socketPath = "/var/run/docker.sock";
+  } else if (dockerClient.uri.startsWith("unix://")) {
+    socketPath = dockerClient.uri.replace("unix://", "");
+  } else {
+    socketPath = "/var/run/docker.sock";
+  }
+
+  return platform === "win32" ? `/${socketPath}` : socketPath;
+};
